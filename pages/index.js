8f685a19616966e076377daa7a90037926a6d7fd
betsegaw <57814519+Betsegaw-hp@users.noreply.github.com>
2021-10-01 20:14:03 +0300

    Create index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home({ posts, users, comments }) {
     <Head>
       <title>Home</title>
       <meta name="keyword" content="next app"></meta>
-      <Script data-ad-client="ca-pub-1355967539894036" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
     </Head>
     <div className={styles.common_container}>
     <div>
