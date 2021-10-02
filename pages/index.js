5be50462084f3a19e747e650139a9da3d545b768
betsegaw <57814519+Betsegaw-hp@users.noreply.github.com>
2021-10-02 14:03:55 +0300

    configure google analytics

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,15 @@ export default function Home({ posts, users, comments }) {
       <title>Home</title>
       <meta name="keyword" content="next app"></meta>
       <script data-ad-client="ca-pub-1355967539894036" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-H9XCS33V2Q"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-H9XCS33V2Q');
+      </script>
     </Head>
     <div className={styles.common_container}>
     <div>
